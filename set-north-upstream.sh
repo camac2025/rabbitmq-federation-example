a11a9beed171f71d8de2ce45126bf340350a0919
camac2025 <camac2025@yahoo.com>
2025-02-28 12:05:17 +0000

    corrected upstream target

--- set-north-upstream.sh
+++ set-north-upstream.sh
@@ -1,5 +1,5 @@
 
 docker exec -i rabbit-north rabbitmqctl clear_policy federation
 docker exec -i rabbit-north rabbitmqctl clear_parameter federation-upstream my-upstream
-docker exec -i rabbit-south rabbitmqctl set_parameter federation-upstream my-upstream '{"uri":"amqp://guest:guest@rabbit-south:5672","expires":3600000}'
+docker exec -i rabbit-south rabbitmqctl set_parameter federation-upstream my-upstream '{"uri":"amqp://guest:guest@rabbit-north:5672","expires":3600000}'
 docker exec -i rabbit-south rabbitmqctl set_policy --vhost "/" --apply-to "all" federation ".*" '{"federation-upstream-set": "all"}'
